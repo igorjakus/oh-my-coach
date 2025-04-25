f5d824023685f275de57592747bdbb9b090e5969
Igor Jakus <igorjakus@protonmail.com>
2025-04-25 23:30:47 +0200

    feat: Update realtime sts

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.routes.health_check import health_check_router
-from app.routes.realtime import realtime_router
+from backend.routes.health_check import health_check_router
+from backend.routes.realtime import realtime_router
 
 app = FastAPI()
 
