b13efa051ae7bbee5068bf4edff31a48ba59b419
Igor Jakus <igorjakus@protonmail.com>
2025-04-25 23:21:50 +0200

    feat: Add realtime speech-to-speech

--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,9 @@
-import asyncio
-import os
+from fastapi import FastAPI
+from app.routes.health_check import health_check_router
+from app.routes.realtime import realtime_router
 
-from agents import ItemHelpers, MessageOutputItem, Runner, set_default_openai_key, trace
-from dotenv import load_dotenv
-from model_agents import (
-    orchestrator_agent,
-    synthesizer_agent,
-)
+app = FastAPI()
 
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
-set_default_openai_key(api_key)
-
-async def main():
-    msg = input("Hi! What would you like translated, and to which languages? ")
-
-    # Run the entire orchestration in a single trace
-    with trace("Orchestrator evaluator"):
-        orchestrator_result = await Runner.run(orchestrator_agent, msg)
-
-        for item in orchestrator_result.new_items:
-            if isinstance(item, MessageOutputItem):
-                text = ItemHelpers.text_message_output(item)
-                if text:
-                    print(f"  - Translation step: {text}")
-
-        synthesizer_result = await Runner.run(synthesizer_agent, orchestrator_result.to_input_list())
-
-    print(f"\n\nFinal response:\n{synthesizer_result.final_output}")
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+# Registering routers
+app.include_router(health_check_router)
+app.include_router(realtime_router)
