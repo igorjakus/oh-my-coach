5feff17f532753b7c0e5c4808ea2661d44ca9a6a
Igor Jakus <igorjakus@protonmail.com>
2025-04-26 02:25:24 +0200

    refactor: remove shit

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
 
-from backend.routes.health_check import health_check_router
-from backend.routes.realtime import realtime_router
-
 app = FastAPI()
 
-# Registering routers
-app.include_router(health_check_router)
-app.include_router(realtime_router)
+
+app.include_router()
