869de6cd688b1bdb098147d262e70c0e6f1efede
Igor Jakus <igorjakus@protonmail.com>
2025-04-26 02:25:24 +0200

    refactor: remove shit

--- backend/config.py
+++ backend/config.py
@@ -2,8 +2,6 @@ import os
 
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
-VENDOR_WS_URL = os.getenv("OPENAI_REALTIME_URL")
 API_KEY = os.getenv("OPENAI_API_KEY")
