4f5835e94825021d5162d4c7cc9502187430f02e
Antoni Goles <antoni.goles@centra.com>
2025-04-26 12:26:34 +0200

    Giga merge with frontend

--- backend/main.py
+++ backend/main.py
@@ -13,11 +13,13 @@ app.include_router(realtime_router)
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from backend.config import create_db_and_tables
 from backend.routes.chat import chat_router
 from backend.routes.health_check import health_check_router
 from backend.routes.personalisation import personalization_router
+from backend.routes.staticFiles import router as StaticFiles_router
 from backend.routes.tasks import task_router
 
 
@@ -28,10 +30,13 @@ async def lifespan(app: FastAPI):
     yield
     # <-- Shutdown logic
 
+
 app = FastAPI(lifespan=lifespan)
 
+static_files = StaticFiles(directory="./public")
+app.mount("/public", static_files, name="public")
 app.include_router(health_check_router)
 app.include_router(chat_router, prefix="/chat", tags=["chat"])
 app.include_router(personalization_router, prefix="/personalization", tags=["personalization"])
 app.include_router(task_router, prefix="/tasks", tags=["tasks"])
->>>>>>> c075ada830a41bd00a68e916a21ed71f8f22da56
+app.include_router(StaticFiles_router)
