d05ee333ce9dcb83c88e0448d6f48e94f6e66061
Igor Jakus <igorjakus@protonmail.com>
2025-04-26 02:25:24 +0200

    refactor: remove shit

--- backend/routes/health_check.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
-
-health_check_router = APIRouter()
-
-
-@health_check_router.get("/")
-async def health_check():
-    return JSONResponse(content={"status": "Iam Alive!"}, status_code=200)
